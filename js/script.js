7a1ac66f6c19ce1e845aaa774573fbd2812c0507
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-09-30 19:43:03 -0300

    feat: Capturando elemento por ID

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Selecionando DIV pelo ID
+let div =
+    document.getElementById('meuDiv');
+
+// Selecionando todos os parágrafos com a classe "paragrafo"
+let paragrafos =
+    document.getElementsByClassName('paragrafo');
+
+// Selecionando o primeiro parágrafo com a classe "paragrafo"
+let primeiroParagrafo =
+    document.querySelector('.paragrafo');
+
+// Selecionando todos os parágrafos dentro do div
+let paragrafoDentroDiv =
+    document.querySelectorAll('#meuDiv p');
