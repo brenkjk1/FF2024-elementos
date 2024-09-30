2065d288b4fd1beea7bb9a6c9d3caf40dc2f9e0c
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-09-30 20:23:16 -0300

    feat: Exemplo de captura de valores em exibição

--- js/script.js
+++ js/script.js
@@ -1,34 +1,10 @@
-// // Selecionando DIV pelo ID
-// let div =
-//     document.getElementById('meuDiv');
 
-// // Selecionando todos os parágrafos com a classe "paragrafo"
-// let paragrafos =
-//     document.getElementsByClassName('paragrafo');
-
-// // Selecionando o primeiro parágrafo com a classe "paragrafo"
-// let primeiroParagrafo =
-//     document.querySelector('.paragrafo');
-
-// // Selecionando todos os parágrafos dentro do div
-// let paragrafoDentroDiv =
-    // document.querySelectorAll('#meuDiv p');
-
-
-// Parte 2
-// Funções para os eventos
-function aoClicar() {
-    alert('Você clicou no Botão!')
+// Parte 3
+function aoClicar () {
+    let campo = document.querySelector('#meuCampo');
+    let valorDigitado = campo.value;
+    alert('Você clicou! e tá escrito "' + valorDigitado + '"');
 }
 
-function aoPerderFoco() {
-    alert('Você saiu do campo!')
-}
-
-// Selecionando o elemento usando querySelector
-let campo = document.querySelector('#meuCampo');
-let botao = document.querySelector('.btnCampo');
-
-// Adicionando os eventos
-campo.addEventListener('blur', aoPerderFoco);
+let botao = document.querySelector('.btnExibaValor');
 botao.addEventListener('click', aoClicar);
