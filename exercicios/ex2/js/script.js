3c1a23f2841d9bfe559f3088afed925f95873b6e
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-09-30 21:59:30 -0300

    feat: ex2 corrigido

--- exercicios/ex2/js/script.js
+++ exercicios/ex2/js/script.js
@@ -1,10 +1,16 @@
-function aoClicar () {
-    let campo = document.querySelector('#numero');
-    let campoDois = document.querySelector('#numero2');
-    let valorDigitado = campo.value;
-    alert('A soma desses valores é:')
+function somar() {
+    let num1 = document.querySelector('#numero').value;
+    let num2 = document.querySelector('#numero2').value;
 
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    let soma = num1 + num2;
+
+    pResult = document.querySelector('#result')
+    pResult.innerHTML = `${num1} + ${num2} = ${soma}`;
+    // let valorDigitado = campo.value;
+    // alert('A soma desses valores é:')
 }
 
-let botao = document.querySelector('.Somar');
-botao.addEventListener('click' aoClicar)
+let botao = document.querySelector("button");
+botao.addEventListener('click', somar);
