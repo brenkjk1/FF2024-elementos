a8b38b7a7ba89f3bc05471013b2b5309c3664a3c
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-09-30 21:35:47 -0300

    feat: commit adicionado ex1

--- exercicios/ex1/js/scriptv2.js
+++ exercicios/ex1/js/scriptv2.js
@@ -2,5 +2,6 @@ function Mensagem () {
     let campo = document.querySelector('#msg')
     let msgDigitada = campo.value;
     alert('A mensagem digitada é: "' + msgDigitada + '"');
+    // alert(`O valor digitado é: "${msgDigitada}"`)
 }
 
