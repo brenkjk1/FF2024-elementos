2b9216d4d3aadb41f8df3e53903a0c993c2bd88c
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-10-02 20:15:52 -0300

    feat: ex9 feito solo e corrigido

---
+++ exercicios/ex9/js/script.js
@@ -0,0 +1,18 @@
+function Verificando() {
+    let height = parseFloat(document.querySelector("#altura").value);
+    let year = parseInt(document.querySelector("#idade").value);
+    let resultado = "";
+
+
+    if (height >= 1.70)
+        resultado = "Pode entrar";
+    else if ((year >= 18) && (height >= 1.60))
+        resultado = "Pode entrar";
+    else
+        resultado = "Não pode entrar"
+
+    document.querySelector("#resultado").innerHTML = resultado;
+}
+
+document.querySelector('button').addEventListener('click', Verificando);
+
