91f89ec219d44c9135c09734fa08e047e383f8b2
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-10-01 21:42:31 -0300

    feat: ex6 corrigido

---
+++ exercicios/ex6/js/script.js
@@ -0,0 +1,15 @@
+function exibirMaior() {
+    let num1 = parseFloat(document.querySelector("#num1").value);
+    let num2 = parseFloat(document.querySelector("#num2").value);
+    let num3 = parseFloat(document.querySelector("#num3").value);
+    let resultado = "";
+    if ((num1 >= num2) && (num1 >= num2))
+        resultado = `${num1} é maior!`
+    else if ((num2 >= num1) && (num2 >= num3))
+        resultado = `${num2} é maior!`
+    else
+        resultado = `${num3} é maior!`
+    document.querySelector("#resultado").innerHTML = resultado;
+}
+
+document.querySelector(".btn").addEventListener('click', exibirMaior);
